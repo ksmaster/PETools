cc50ae8b4bd64bdfd382100e20a363cab96289da
wcj <695854766@qq.com>
2023-03-25 13:19:21 +0800

    ddd

--- peloadworker.cpp
+++ peloadworker.cpp
@@ -11,6 +11,10 @@ PELoadWorker::PELoadWorker(CPEInfo &peInfo, QObject *parent) : QObject(parent),m
 void PELoadWorker::loadPEBasic(const QString &fileName)
 {
     qDebug() << "loadPEBasic: " << fileName  << ", thread id: " << QThread::currentThreadId();
-    m_peInfoRef.LoadPE(reinterpret_cast<LPCTSTR>(fileName.utf16()));
+#if defined(WIN32)
+     m_peInfoRef.LoadPE(reinterpret_cast<LPCTSTR>(fileName.utf16()));
+#elif __linux__
+     m_peInfoRef.LoadPE(fileName.toUtf8().constData());
+#endif
     emit startShowPEInfoOnGUI();
 }
